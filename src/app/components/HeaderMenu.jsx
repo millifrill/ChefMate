dcd1d0a7fcc2497107f0a3fcdd858dbcd1750bdb
Camilla <73700387+millifrill@users.noreply.github.com>
2024-03-26 23:57:51 +0100

    added drawer to app bar

--- src/app/components/HeaderMenu.jsx
+++ src/app/components/HeaderMenu.jsx
@@ -1,9 +1,18 @@
 'use client';
 
 import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
@@ -41,7 +50,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 	width: '100%',
 	'& .MuiInputBase-input': {
 		padding: theme.spacing(1, 1, 1, 0),
-		// vertical padding + font size from searchIcon
 		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
 		transition: theme.transitions.create('width'),
 		[theme.breakpoints.up('sm')]: {
@@ -53,34 +61,75 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 	},
 }));
 
-export default function HeaderMenu() {
-	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position='static'>
-				<Toolbar>
-					<IconButton
-						size='large'
-						edge='start'
-						color='inherit'
-						aria-label='open drawer'
-						sx={{ mr: 2 }}>
-						<MenuIcon />
-					</IconButton>
-					<Typography
-						variant='h5'
-						noWrap
-						component='div'
-						sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
-						ChefMate
-					</Typography>
-					<Search>
-						<SearchIconWrapper>
-							<SearchIcon />
-						</SearchIconWrapper>
-						<StyledInputBase placeholder='Search…' inputProps={{ 'aria-label': 'search' }} />
-					</Search>
-				</Toolbar>
-			</AppBar>
+export default function CombinedComponent() {
+	const [open, setOpen] = React.useState(false);
+
+	const toggleDrawer = (newOpen) => () => {
+		setOpen(newOpen);
+	};
+
+	const DrawerList = (
+		<Box sx={{ width: 250 }} role='presentation' onClick={toggleDrawer(false)}>
+			<List>
+				{['Toggle theme'].map((text, index) => (
+					<ListItem key={text} disablePadding>
+						<ListItemButton>
+							<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItemButton>
+					</ListItem>
+				))}
+			</List>
+			<Divider />
+			<List>
+				{['Recepies', 'Contact'].map((text, index) => (
+					<ListItem key={text} disablePadding>
+						<ListItemButton>
+							<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItemButton>
+					</ListItem>
+				))}
+			</List>
 		</Box>
+	);
+
+	return (
+		<div>
+			<Box sx={{ flexGrow: 1 }}>
+				<AppBar position='static'>
+					<Toolbar>
+						<IconButton
+							size='large'
+							edge='start'
+							color='inherit'
+							aria-label='open drawer'
+							onClick={toggleDrawer(true)}
+							sx={{ mr: 2 }}>
+							<MenuIcon />
+						</IconButton>
+						<Typography
+							variant='h5'
+							noWrap
+							component='div'
+							sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+							ChefMate
+						</Typography>
+						<Search>
+							<SearchIconWrapper>
+								<SearchIcon />
+							</SearchIconWrapper>
+							<StyledInputBase
+								placeholder='Search…'
+								inputProps={{ 'aria-label': 'search' }}
+							/>
+						</Search>
+					</Toolbar>
+				</AppBar>
+			</Box>
+			<Drawer open={open} onClose={toggleDrawer(false)}>
+				{DrawerList}
+			</Drawer>
+		</div>
 	);
 }
