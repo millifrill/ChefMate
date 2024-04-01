63b55a8c219315e0d21bda1705b372c3c341eae4
Camilla <73700387+millifrill@users.noreply.github.com>
2024-04-01 21:29:44 +0200

    added margin

--- src/app/components/RecipeCard.jsx
+++ src/app/components/RecipeCard.jsx
@@ -5,26 +5,40 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea, Box } from '@mui/material';
 
-export default function RecipeCard() {
+export default function RecipeCard({ recipe }) {
 	return (
-		<Card sx={{ maxWidth: 345, backgroundColor: '#000' }}>
+		<Card
+			sx={{
+				width: 300,
+				backgroundColor: '#000',
+				margin: 'auto',
+			}}>
 			<CardActionArea>
 				<CardMedia
 					component='img'
-					height='140'
-					image='/static/images/cards/contemplative-reptile.jpg'
-					alt='green iguana'
+					height='160'
+					width='140'
+					image={recipe?.strMealThumb}
+					alt={recipe?.strMeal}
 				/>
 				<CardContent>
-					<Typography gutterBottom variant='h5' component='div' color='#fff'>
-						Pasta with salmon
+					<Typography
+						gutterBottom
+						variant='h5'
+						component='div'
+						color='#fff'
+						sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+						{recipe?.strMeal}
 					</Typography>
 					<Box display='flex' justifyContent='space-between'>
-						<Typography variant='body2' color='#fff'>
-							30 min
+						<Typography variant='body2' color='#fff' marginRight={'15px'}>
+							{recipe?.strCategory || 'No category'}
 						</Typography>
-						<Typography variant='body2' color='#fff'>
-							Fisk
+						<Typography
+							variant='body2'
+							color='#fff'
+							sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+							{recipe?.strTags || 'No tags'}
 						</Typography>
 					</Box>
 				</CardContent>
