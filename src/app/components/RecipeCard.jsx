ce12efbdfca2ee85dc1e03182f5d157b1242a379
Camilla <73700387+millifrill@users.noreply.github.com>
2024-04-02 23:52:51 +0200

    added link to recipes full details page

--- src/app/components/RecipeCard.jsx
+++ src/app/components/RecipeCard.jsx
@@ -4,45 +4,48 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea, Box } from '@mui/material';
+import Link from 'next/link';
 
 export default function RecipeCard({ recipe }) {
 	return (
-		<Card
-			sx={{
-				width: 300,
-				backgroundColor: '#000',
-				margin: 'auto',
-			}}>
-			<CardActionArea>
-				<CardMedia
-					component='img'
-					height='160'
-					width='140'
-					image={recipe?.strMealThumb}
-					alt={recipe?.strMeal}
-				/>
-				<CardContent>
-					<Typography
-						gutterBottom
-						variant='h5'
-						component='div'
-						color='#fff'
-						sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
-						{recipe?.strMeal}
-					</Typography>
-					<Box display='flex' justifyContent='space-between'>
-						<Typography variant='body2' color='#fff' marginRight={'15px'}>
-							{recipe?.strCategory || 'No category'}
-						</Typography>
+		<Link href={'/recipes/${recipe?.idMeal}'}>
+			<Card
+				sx={{
+					width: 300,
+					backgroundColor: '#000',
+					margin: 'auto',
+				}}>
+				<CardActionArea>
+					<CardMedia
+						component='img'
+						height='160'
+						width='140'
+						image={recipe?.strMealThumb}
+						alt={recipe?.strMeal}
+					/>
+					<CardContent>
 						<Typography
-							variant='body2'
+							gutterBottom
+							variant='h5'
+							component='div'
 							color='#fff'
 							sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
-							{recipe?.strTags || 'No tags'}
+							{recipe?.strMeal}
 						</Typography>
-					</Box>
-				</CardContent>
-			</CardActionArea>
-		</Card>
+						<Box display='flex' justifyContent='space-between'>
+							<Typography variant='body2' color='#fff' marginRight={'15px'}>
+								{recipe?.strCategory || 'No category'}
+							</Typography>
+							<Typography
+								variant='body2'
+								color='#fff'
+								sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+								{recipe?.strTags || 'No tags'}
+							</Typography>
+						</Box>
+					</CardContent>
+				</CardActionArea>
+			</Card>
+		</Link>
 	);
 }
