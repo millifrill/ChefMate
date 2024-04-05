537cef4048a2f8b2ea26478115d4eeffb360fa3b
Camilla <73700387+millifrill@users.noreply.github.com>
2024-04-05 23:59:14 +0200

    added ThemeProvider

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,29 @@
 'use client';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import RecipesBanner from './components/RecipesBanner';
 import { Main, MainBox } from './global-styles';
 import Recipes from './recipes/page';
+import { useState } from 'react';
+// import { theme, toggleTheme, toggleDarkMode } from './ThemeProvider';
 
 export default function Home() {
+	const [mode, setMode] = useState(false);
+
+	const darkTheme = createTheme({
+		palette: {
+			mode: 'dark',
+		},
+	});
+
 	return (
-		<>
+		<ThemeProvider theme={darkTheme}>
 			<RecipesBanner />
 			<Main>
 				<MainBox>
 					<Recipes />
 				</MainBox>
 			</Main>
-		</>
+		</ThemeProvider>
 	);
 }
